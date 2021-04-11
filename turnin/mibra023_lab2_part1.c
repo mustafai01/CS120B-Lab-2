fc3ae05b1251695c63a038579877cd4609f4f8e8
mustafai01 <mustafai01@icloud.com>
2021-04-10 18:47:47 -0700

    Completed part 1

--- turnin/mibra023_lab2_part1.c
+++ turnin/mibra023_lab2_part1.c
@@ -1,11 +1,11 @@
-/*	Author: lab
+/*      Author: lab
  *  Partner(s) Name: Mustafa Ibrahim
- *	Lab Section: 021
- *	Assignment: Lab #2  Exercise #1
- *	Exercise Description: [optional - include for your own benefit]
+ *      Lab Section: 021
+ *      Assignment: Lab #2  Exercise #1
+ *      Exercise Description: [optional - include for your own benefit]
  *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
+ *      I acknowledge all content contained herein, excluding template or example
+ *      code, is my own original work.
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
@@ -15,20 +15,20 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
-    DDRB = 0xFF; PORTB = 0x00; 
-    unsigned char tmpB = 0x00; 
-    unsigned char tmpA0 = 0x00; 
-    unsigned char tmpA1 = 0x00;
+    DDRB = 0xFF; PORTB = 0x00;
+    unsigned char tmpB = 0x00;
+    unsigned char tmpA = 0x00;
     /* Insert your solution below */
     while (1) {
-	tmpA0 = PINA & 0x01; tmpA1 = PINA & 0x10;
-	if ((tmpA0 == 0x01) && (tmpA1 == 0x00)) { // True if PA0 is 1
-           tmpB = (tmpB & 0xFE) | 0x01; 
-	}
-       	else {
-	   tmpB = (tmpB & 0xFE) | 0x002; 
-	}	
-        PORTB = tmpB;	
+        tmpA = PINA & 0x03; 
+        if (tmpA == 0x01) { 
+           tmpB = 0x01;
+        }
+        else {
+           tmpB = 0x00;
+        }
+        PORTB = tmpB;
     }
     return 1;
 }
+
